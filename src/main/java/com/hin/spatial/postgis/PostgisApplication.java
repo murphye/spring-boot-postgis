62685cf2aa29b8949054cbec6a7d752d5eb5ff47
Eric Murphy <murphye@gmail.com>
2022-10-01 11:05:48 -0700

    User org.n52

--- src/main/java/com/hin/spatial/postgis/PostgisApplication.java
+++ src/main/java/com/hin/spatial/postgis/PostgisApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import org.n52.jackson.datatype.jts.JtsModule;
 
 @SpringBootApplication
 public class PostgisApplication {
