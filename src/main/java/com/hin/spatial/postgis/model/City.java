85df79c71e4e1a48ed23fd072cc9c24818d3579a
Eric Murphy <murphye@gmail.com>
2022-10-01 10:19:54 -0700

    Update City.java

--- src/main/java/com/hin/spatial/postgis/model/City.java
+++ src/main/java/com/hin/spatial/postgis/model/City.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import org.hibernate.annotations.Type;
-
 import org.locationtech.jts.geom.Point;
 
 import lombok.Data;
